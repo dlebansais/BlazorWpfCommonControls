e11c650d94201eeb3d49500b0b6995c5c26a3516
David Le Bansais <dlebansais@gmail.com>
2023-11-02 21:21:59 +0100

    Test fix.

--- Test/BlazorWpfCommonControls.Test/EyeCircle/TestEyeCirclePanel.cs
+++ Test/BlazorWpfCommonControls.Test/EyeCircle/TestEyeCirclePanel.cs
@@ -18,37 +18,37 @@ public partial class TestEyeCirclePanel
 
             BidimensionalArray<EyeCircleType> TypeArray = new(4, 1, EyeCircleType.Empty);
             TypeArray.Set(0, 0, EyeCircleType.Open);
-            TypeArray.Set(0, 1, EyeCircleType.Closed);
-            TypeArray.Set(0, 2, EyeCircleType.Mixed);
-            TypeArray.Set(0, 3, EyeCircleType.Empty);
+            TypeArray.Set(1, 0, EyeCircleType.Closed);
+            TypeArray.Set(2, 0, EyeCircleType.Mixed);
+            TypeArray.Set(3, 0, EyeCircleType.Empty);
             Panel.TypeArray = TypeArray;
 
             BidimensionalArray<Brush> ForegroundArray = new(4, 1, Brushes.Transparent);
             ForegroundArray.Set(0, 0, Brushes.Red);
-            ForegroundArray.Set(0, 1, Brushes.Green);
-            ForegroundArray.Set(0, 2, Brushes.Blue);
-            ForegroundArray.Set(0, 3, Brushes.Transparent);
+            ForegroundArray.Set(1, 0, Brushes.Green);
+            ForegroundArray.Set(2, 0, Brushes.Blue);
+            ForegroundArray.Set(3, 0, Brushes.Transparent);
             Panel.ForegroundArray = ForegroundArray;
 
             BidimensionalArray<string?> ToolTipArray = new(4, 1, null);
             ToolTipArray.Set(0, 0, "Red");
-            ToolTipArray.Set(0, 1, "Green");
-            ToolTipArray.Set(0, 2, "Blue");
-            ToolTipArray.Set(0, 3, null);
+            ToolTipArray.Set(1, 0, "Green");
+            ToolTipArray.Set(2, 0, "Blue");
+            ToolTipArray.Set(3, 0, null);
             Panel.ToolTipArray = ToolTipArray;
 
             BidimensionalArray<bool> MaskArray = new(4, 1, false);
             MaskArray.Set(0, 0, true);
-            MaskArray.Set(0, 1, true);
-            MaskArray.Set(0, 2, true);
-            MaskArray.Set(0, 3, false);
+            MaskArray.Set(1, 0, true);
+            MaskArray.Set(2, 0, true);
+            MaskArray.Set(3, 0, false);
             Panel.MaskArray = MaskArray;
 
             BidimensionalArray<bool> HighlightArray = new(4, 1, false);
             HighlightArray.Set(0, 0, true);
-            HighlightArray.Set(0, 1, true);
-            HighlightArray.Set(0, 2, true);
-            HighlightArray.Set(0, 3, false);
+            HighlightArray.Set(1, 0, true);
+            HighlightArray.Set(2, 0, true);
+            HighlightArray.Set(3, 0, false);
             Panel.HighlightArray = HighlightArray;
 
             Thread.Sleep(100);
