cca82f990c76bd63b0554ca5ce4065bd22469586
David Le Bansais <dlebansais@gmail.com>
2023-11-04 20:17:09 +0100

    Added HtmlTextBlock.

---
+++ Test/BlazorWpfCommonControls.Test/HtmlTextBlock/TestHtmlTextBlock.Coverage.cs
@@ -0,0 +1,27 @@
+namespace BlazorWpfCommonControls.Test;
+
+using CustomControls.BlazorWpfCommon;
+using NUnit.Framework;
+
+public partial class TestHtmlTextBlock
+{
+    [Test]
+    public void TestNullText()
+    {
+        bool Success = TestTools.StaThreadWrapper(() =>
+        {
+            HtmlTextBlock Control = new();
+
+            Control.HtmlFormattedText = "Test";
+            Assert.That(Control.HtmlFormattedText, Is.EqualTo("Test"));
+            
+            Control.HtmlFormattedText = null;
+            Assert.That(Control.HtmlFormattedText, Is.Null);
+
+            var Popup = TestTools.LoadControl(Control);
+            TestTools.UnloadControl(Popup);
+        });
+
+        Assert.IsTrue(Success);
+    }
+}
